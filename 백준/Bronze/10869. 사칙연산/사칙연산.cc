7d6e57cc194f781a9d5de7905bb63fb03bcd73aa
sk-choi <80041090+sk-choi@users.noreply.github.com>
2022-07-15 01:31:28 +0900

    [Bronze V] Title: 사칙연산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A;
+    int B;
+    cin >> A >> B;
+    
+    cout << A + B << endl;
+    cout << A - B << endl;
+    cout << A * B << endl;
+    cout << A / B << endl;
+    cout << A % B;
+    
+    return 0;
+    
+}
