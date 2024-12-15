3feb95471d6f096b15a869e41ba709634d800220
sk-choi <80041090+sk-choi@users.noreply.github.com>
2024-12-15 10:15:29 +0900

    [Bronze IV] Title: $A + B - C$, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.py
@@ -0,0 +1,8 @@
+num1 = input()
+num2 = input()
+num3 = input()
+
+result01 = int(num1) + int(num2) - int(num3);
+result02 = int(str(num1) + str(num2)) - int(num3);
+print(result01);
+print(result02);
